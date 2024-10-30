a1b10cce6737d65fcc21e84ff1e160b06e0c213d
Nashid K <kp.nashid.nashid@gmail.com>
2024-10-30 22:12:09 +0530

    updated

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Make sure this matches your backend's base URL
+  baseURL: process.env.REACT_APP_API_URL,
 });
+
 
 export default api;
